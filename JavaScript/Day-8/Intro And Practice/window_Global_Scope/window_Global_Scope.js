9e61cd5d90cee6460fd1bd983cac81358e278d82
Rao Khuram Shahzad <krshahzad7@gmail.com>
2023-01-30 16:53:07 +0500

    window global object- day8

---
+++ JavaScript/Day-8/Intro And Practice/window_Global_Scope/window_Global_Scope.js
@@ -0,0 +1,7 @@
+// variable declared without datatypes are globally scoped variables
+//example : 1
+
+a = 30;
+b = 'JavaScript';
+
+// now try to access these in browsers console, without loging, just write the a,b and tou will be able to access those variables, as these are global scoped in window object.
