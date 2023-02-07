2f24ce7726c0c0f39e8d464d32e66fa220b15ce8
Rao Khuram Shahzad <krshahzad7@gmail.com>
2023-02-07 19:46:04 +0500

    done with the intro of destructuring

--- JavaScript/Day-11/Intro And Practice/Destructuring Intro.js
+++ JavaScript/Day-11/Intro And Practice/Destructuring Intro.js
@@ -173,3 +173,128 @@ console.log(d);
 // Destructuring keys as functions parameters:
 
 //EXP:  1.
+
+// we will do this without destructuring
+
+const rect2 = {
+    width : 10,
+    height: 20,
+}
+
+//  now create a function which returns the perimeter.
+const calcPerimeter = (shape) => {
+    return 2 * (shape.width + shape.height);
+}
+
+console.log(calcPerimeter(rect2));
+
+
+
+// EXP: 2 Dificult one :(
+
+const person = {
+    fName : 'Khuram',
+    lName : 'Shahzad',
+    age : 22,
+    country : 'Pakistan',
+    job : 'Student',
+    skills : [
+        'HTML',
+        'CSS',
+        'JS Continue...',
+        'Will learn More',
+    ],
+    languages : ['Urdu', 'English'],
+}
+
+// Now create a function which give info about the person without destructuring.
+const getPersonInfo = (obj) => {
+    const skill = obj.skills;
+    const skillFormatted = obj.skills.slice(0, -1).join(',');
+
+    const lang = obj.languages;
+    const langFormatted = obj.languages.slice(0, -1).join(',');
+
+    let personInfo = `${obj.fName} ${obj.lName} live in ${obj.country}. He is ${obj.age}.  He is an ${obj.job}. He learn ${skillFormatted} and ${skill.slice(-1)}. He speaks ${langFormatted} and ${lang.slice(-1)} languages.`
+
+    return personInfo;
+}
+
+console.log(getPersonInfo(person));
+
+
+
+//       => OBJECT PARAMETER WITH DESTRUCTURING
+// With destrcuting.
+
+// EXP : 1-
+const rect3 = {
+    width : 5,
+    height : 10,
+}
+
+const calcPerimeter2 = ({width : w, height : h}) => {
+    return 2 * (w + h);
+}
+
+console.log(calcPerimeter(rect3));
+
+
+//EXP : 2-
+const person2 = {
+    fName : 'Khuram',
+    lName : 'Shahzad',
+    age : 22,
+    country : 'Pakistan',
+    job : 'Student',
+    skills : [
+        'HTML',
+        'CSS',
+        'JS Continue...',
+        'Will learn More',
+    ],
+    languages : ['Urdu', 'English'],
+}
+
+const getPersonInfoDestructure = ({fName, lName, age, country, job, skills, languages}) => {
+    let skill = skills;
+    let skillFormatted = skill.slice(0, -1).join(', ');
+
+    let lang = languages;
+    let langFormatted = lang.slice(0, -1).join(', ');
+
+    return `${fName} ${lName} is ${age} years old ${country}i. He is a ${job} and also skilled in ${skillFormatted} and ${skill.slice(-1)}. He is fluent in ${langFormatted} and ${lang.slice(-1)}`
+}
+
+console.log(getPersonInfoDestructure(person2));
+
+
+
+
+
+
+// =>   DESTRUCTURING OBJECT DURING ITERATIONS.
+
+//  we can destructure objects while iteratiing through them using fof loop.
+
+const todoList = [
+    {
+      task:'Prepare JS Test',
+      time:'4/1/2020 8:30',
+      isCompleted:true
+    },
+    {
+      task:'Give JS Test',
+      time:'4/1/2020 10:00',
+      isCompleted:false
+    },
+    {
+      task:'Assess Test Result',
+      time:'4/1/2020 1:00',
+      isCompleted:false
+    }
+    ]
+
+for (const {task, time, isCompleted} of todoList) {
+    console.log(task, time, isCompleted);
+}
