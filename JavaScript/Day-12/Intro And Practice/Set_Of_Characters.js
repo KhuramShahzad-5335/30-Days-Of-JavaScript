0940ac9ebbc625ebf5cca8bb45e1ef26464871fd
Rao Khuram Shahzad <krshahzad7@gmail.com>
2023-02-10 19:53:07 +0500

    will end regExp today

--- JavaScript/Day-12/Intro And Practice/Set_Of_Characters.js
+++ JavaScript/Day-12/Intro And Practice/Set_Of_Characters.js
@@ -12,6 +12,61 @@
 // \: Uses to escape special characters.
     // \d :  matches for strings containing numbers.
     // \D :  matches for strings does not contain numbers.
+    // \D :  matches for strings does not contain numbers.
 
 
 // . : any character except new line character.
+
+
+// ^ : Starts with :
+    //  r'^substring' eg r'^love', a sentance which starts with a word love.
+    //  r'[^abc]' : means any character starting with a,b or c.
+
+
+// $: : Ends with :
+    // r'substring$' eg r'love$', a sentance which ends with the word love.
+
+
+// * : Zero or more times :
+    // r'[a]*' , means a is optional or it can appear many times.
+
+
+// +: one or more times:
+    // r'[a]+ : means a can appear once or more than once.
+
+
+// ? : Zero or one times:
+    // r'[a]?' means atleast once or more times appearance.
+
+
+// \b : word bounder, matches with the beginning or ending of a word.
+
+
+// {3} :  Means exactly 3 characters.
+// {3, } :  Means 3 or more characters.
+// {3, 8} :  Means characters between 3 or 8.
+
+
+// |:  : either or:
+    // r'apple | banana' means either apple or banana.
+
+
+// () : Capture and group.
+
+
+
+
+
+
+
+//***  LETS SEE SOME EXAMPLES TO CLARIFY ABOVE META CHARACTERS :
+
+
+// EXP : 1 : Lets use square brackets to include lower or upper case in apple regExp.
+
+let pattern = '[Aa]pple'     // this bracket pair means either A or a.
+let txt = ' Apple and banana both are fruits.'
+
+let matches = txt.match(pattern);
+
+console.log(matches);
