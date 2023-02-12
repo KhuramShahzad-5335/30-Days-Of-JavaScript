3cf3e4cccda8b908873534230b108e9fce14d946
Rao Khuram Shahzad <krshahzad7@gmail.com>
2023-02-13 02:55:17 +0500

    END  THE PRACTICE AFTER THE LONG TIME

--- JavaScript/Day-12/Intro And Practice/Set_Of_Characters.js
+++ JavaScript/Day-12/Intro And Practice/Set_Of_Characters.js
@@ -70,3 +70,173 @@ let txt = ' Apple and banana both are fruits.'
 let matches = txt.match(pattern);
 
 console.log(matches);
+
+
+
+
+
+
+// EXP : 2 : Escape character(\) in RegExp.
+
+pattern = /\d/g;        // \d is special char which means digits.
+txt = 'This is testing txt, written in 2023, 1/ 10';
+
+matches = txt.match(pattern);      // this metod returns aray of digits
+console.log(matches); 
+
+
+
+
+// EXP : 3 : ONE OR MORE TIMES : 
+
+// to get full pair of digits or grp or digits we do this
+pattern = /\d+/g          // \d+ will get full grp of digits.
+
+matches = txt.match(pattern);
+console.log(matches);
+
+
+
+
+// EXP : 4 : PERIOD .   :
+//  period means any char to initial character for once except new line.
+
+pattern = /[a].]/g          // a and . means any char except new line
+txt = 'Apple and banana both are fruits.'
+
+matches = txt.match(pattern);
+console.log(matches)
+
+
+
+// Now to get full txt we will use, + operaor which selects any char one or more times.
+
+pattern = /[a].+/g ;
+
+matches = txt.match(pattern);
+console.log(matches);
+
+
+
+
+
+
+
+// EXP : 5 : (*) 0 OR MORE TIMES :
+// ZERO OR MANY TIMES :  The pattern may not occur or may occur many times.
+
+
+pattern = /[a].*/g;
+txt = 'Apple and banana are sweet fruits.';     //. any char except next line, + any character once or many times 
+
+matches = txt.match(pattern);
+console.log(matches);
+
+
+
+
+
+
+
+
+// EXP : 6 : 0 or once ?/
+pattern = /[Ee]-?mail/g;    // which means mail is optional. // Ee any can appear.
+txt = 'I am not sure if there is a convention how to write the word e-mail.\
+Some people write it email others may write it as Email or E-mail.';
+
+matches = txt.match(pattern);
+console.log(matches);
+
+
+
+
+
+
+
+
+
+// EXP : 6 : QUANTIFIER IN REGEXP : 
+//  We can speify the length of the substring we are looking for in a text, using curly braces
+//  lets imaging we are interested in a substring which has exactly 4 characters.
+
+pattern = /\b\w{4}\b/g        //  exactly strign of 4 characters long.
+txt = 'This regular expression example was made in December 6,  2019.';
+
+matches = txt.match(pattern);
+console.log(matches);
+
+
+
+// Exactly four characters string without numbers.
+pattern = /\b[a-zA-Z]{4}\b/g
+
+matches = txt.match(pattern);
+console.log(matches);
+
+
+
+
+
+
+
+
+// EXP : 7 : CART ^
+// ^ starts with returns an array of substring from which the text starts.
+txt = 'this is text to check the working of starts with meta character.'
+pattern = /^[Tt]his/;
+
+matches = txt.match(pattern);
+console.log(matches);
+
+
+// If ^ is in set than it negates, lets see example.
+txt = 'This regular expression example was made in December 6,  2019.'
+pattern = /[^a-zA-Z,. ]+/g     // means not a-z,A-Z no comma periood or space + contains all the character except the negated
+matches = txt.match(pattern);
+console.log(matches);
+
+
+
+
+
+
+
+// EXP : 8 : NEGATION :
+//  Negation will check for the matches and will return the opposite things.
+
+txt = 'This regular expression example was made in December 6,  2019.'
+
+//  Now neglect the text and return only numbers
+pattern = /[^a-zA-Z,. ]+/g;
+
+matches = txt.match(pattern);
+console.log(matches);
+
+
+
+
+// EXP : 8 : EXACT MATCH:
+// To match all the words it should have ^ in the starting and $ in the ending.
+
+txt = 'Asabeneh'
+
+// now we will try to match if the pattern has all the elements in the txt string.
+pattern = /^[a-z][A-Z]{3,12}$/;
+
+matches = pattern.test(txt);
+console.log(matches);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=======================       THE END        ====================
