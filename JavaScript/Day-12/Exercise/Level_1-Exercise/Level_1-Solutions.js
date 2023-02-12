f5d4bc11ce3f4f582c2262b1f1bd0cd48d4d7366
Rao Khuram Shahzad <krshahzad7@gmail.com>
2023-02-13 03:10:15 +0500

    started exercise

---
+++ JavaScript/Day-12/Exercise/Level_1-Exercise/Level_1-Solutions.js
@@ -0,0 +1,17 @@
+// ======  REUSABLE VARIABLES:
+
+let txt = '',
+pattern;
+
+
+
+//=====================================================
+
+
+
+//==================            1.
+//Calculate the total annual income of the person from the following text. ‘He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.’
+
+txt = 'He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month'
+
+//  STEP : 2 - 
