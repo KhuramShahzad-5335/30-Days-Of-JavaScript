217f967d1946863a25e97f91f0061e97d8948029
Rao Khuram Shahzad <krshahzad7@gmail.com>
2023-02-17 22:52:14 +0500

    Level 2 exercise completed

---
+++ JavaScript/Day-13/Exercises/Level_2-Exercise/Level_2-Solutions.js
@@ -0,0 +1,19 @@
+//==============            1.
+//10 > 2 * 10 use console.assert()
+
+console.assert(10 > 2 * 10, 'Error this expression is fucked up :(');
+
+
+
+//==============            2.
+// Write a warning message using console.warn()
+
+console.warn('The above expression is returning false. Fix it :)');
+
+
+
+
+//=============             3.
+// Write an error message using console.error().
+
+console.error('10 is not greater than 10 * 2. \n Ok ?')
