a3f4c12b06859a4ca203fab47de71c7cb84a35a9
Rao Khuram Shahzad <krshahzad7@gmail.com>
2023-02-22 16:08:48 +0500

    introduced with the web storage methods

---
+++ JavaScript/Day-17/Intro And Practice/Web_Storage_Html5_Objects.js
@@ -0,0 +1,33 @@
+//=========             HTML-5 WEB STORAGE OBJECTS              ============
+
+/*
+        HTML5 provides 2 - objects for storing data on the client : 
+
+        1.  window.localStorage  : 
+                This method stores data with no expiration date.
+        2.  window.sessionStorage : 
+                This method stores data for one session (Data will be lost
+            when the browser tab is closed). Most modern web browsers support
+            the web storage, however it is good to check browser support for 
+            local and session storage. 
+
+        *** Let us see the available methods for web storage objects :
+
+
+        // ===== > WEB STORAGE OBJECT METHODS : -
+
+        Methods are discussed below :
+
+        1. localStorge :  To display local storgae object.
+        2. localStorage.clear() : - To remove everything in the localStorage.
+        3. localStorage.setItem() : - To store data in local storage. This method
+                Takes keys as a parameters.
+        4. localStorage.getItem() : - To display data stored in the local-
+                Storage. It also takes key as parameters.
+        5. localStorage.removeItem : - To remove stored item from the local-
+                Storage. It also takes key as a perameters.
+        6. localStorag.key() : - To display data stored in local storage. It
+                takes index as the parameter.
+ 
+
+*/
